d21235c49176c77606c5d8efe3bd220abd8c18a3
Balakrishna K <krish172001@gmail.com>
2021-07-16 17:06:03 -0600

    update

--- mern-auth/config/keys.js
+++ mern-auth/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://balakrishna:bk1234@mycluster.ebqu7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv:",
     secretOrKey: "secret"
   };
